bd342a02df098d70a8de643eeadd5e95c8026f86
Nguyen Hoang Chuong <chuong.nguyen@kobiton.com>
2021-06-18 14:24:41 +0700

    Add Subversion 1.13 formula

---
+++ Formula/x264@r3048.rb
@@ -0,0 +1,64 @@
+class X264@r3048 < Formula
+  desc "H.264/AVC encoder"
+  homepage "https://www.videolan.org/developers/x264.html"
+  license "GPL-2.0-only"
+  head "https://code.videolan.org/videolan/x264.git"
+
+  stable do
+    # the latest commit on the stable branch
+    url "https://code.videolan.org/videolan/x264.git",
+        revision: "b86ae3c66f51ac9eab5ab7ad09a9d62e67961b8a"
+    version "r3048"
+  end
+
+  # There's no guarantee that the versions we find on the `release-macos` index
+  # page are stable but there didn't appear to be a different way of getting
+  # the version information at the time of writing.
+  livecheck do
+    url "https://artifacts.videolan.org/x264/release-macos/"
+    regex(%r{href=.*?x264[._-](r\d+)[._-][\da-z]+/?["' >]}i)
+  end
+
+  depends_on "nasm" => :build
+
+  if MacOS.version <= :high_sierra
+    # Stack realignment requires newer Clang
+    # https://code.videolan.org/videolan/x264/-/commit/b5bc5d69c580429ff716bafcd43655e855c31b02
+    depends_on "gcc"
+    fails_with :clang
+  end
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-lsmash
+      --disable-swscale
+      --disable-ffms
+      --enable-shared
+      --enable-static
+      --enable-strip
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s.delete("r"), shell_output("#{bin}/x264 --version").lines.first
+    (testpath/"test.c").write <<~EOS
+      #include <stdint.h>
+      #include <x264.h>
+
+      int main()
+      {
+          x264_picture_t pic;
+          x264_picture_init(&pic);
+          x264_picture_alloc(&pic, 1, 1, 1);
+          x264_picture_clean(&pic);
+          return 0;
+      }
+    EOS
+    system ENV.cc, "-L#{lib}", "test.c", "-lx264", "-o", "test"
+    system "./test"
+  end
+end
